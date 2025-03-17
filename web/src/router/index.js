dc48807c2e3133a98a887a056a5b43edb943ccd8
wenxiao Lv <2064747320@qq.com>
2025-03-17 16:33:45 +0800

    build the frame

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PkIndexView from '../views/pk/PkIndexView.vue'
+import RecordIndexView from '../views/record/RecordIndedxView.vue'
+import RankListIndexView from '../views/ranklist/RankListIndexView.vue'
+import UserBotIndexView from '../views/user/bots/UserBotIndexView.vue'
+import NotFoundView from '../views/error/NotFoundView.vue'
 
 const routes = [
-
+  {
+    path: "/",
+    name: "home",
+    redirect: "/pk/",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PkIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RankListIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFoundView,
+  },
+  {
+    path: "/:catAll(.*)",
+    redirect: "/404/",
+  }
 ]
 
 const router = createRouter({
