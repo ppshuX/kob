411a308eae8d1c64adc0c3d5df88a2c2b103e457
wenxiao Lv <2064747320@qq.com>
2025-03-25 20:59:19 +0800

    实现微服务：匹配系统（中）

--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -1,11 +1,13 @@
 <template>
    <PlayGround v-if="$store.state.pk.status === 'playing'" />
    <MatchGround v-if="$store.state.pk.status === 'matching'" />
+   <ResultBoard v-if="$store.state.pk.loser != 'none'"/>
 </template>
 
 <script>
 import PlayGround from '../../components/PlayGround.vue'
 import MatchGround from '@/components/MatchGround.vue';
+import ResultBoard from '../../components/ResultBoard.vue'
 import { onMounted, onUnmounted } from 'vue';
 import { useStore } from 'vuex';
 
@@ -13,6 +15,7 @@ export default {
     components: {
         PlayGround,
         MatchGround,
+        ResultBoard,
     },
     setup() {
         const store = useStore();
@@ -21,7 +24,7 @@ export default {
         let socket = null;
         onMounted(() => {
             store.commit("updateOpponent", {
-                username: "未知",
+                username: "未知对手",
                 photo: "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png",
             })
             socket = new WebSocket(socketUrl);
@@ -40,8 +43,26 @@ export default {
                     });
                     setTimeout(() => {
                         store.commit("updateStatus", "playing");
-                    }, 2000);
-                    store.commit("updateGamemap", data.gamemap);
+                    }, 1000);
+                    store.commit("updateGamemap", data.game);
+                } else if (data.event === "move") {
+                    console.log(data);
+                    const game = store.state.pk.gameObject;
+                    const [snake0, snake1] = game.snakes;
+                    snake0.set_direction(data.a_direction);
+                    snake1.set_direction(data.b_direction);
+                } else if (data.event === "result") {
+                    console.log(data);
+                    const game = store.state.pk.gameObject;
+                    const [snake0, snake1] = game.snakes;
+
+                    if (data.loser === "all" || data.loser === "A") {
+                        snake0.status = "die";
+                    }
+                    if (data.loser === "all" || data.loser === "B") {
+                        snake1.status = "die";
+                    }
+                    store.commit("updateLoser", data.loser);
                 }
             }
 
