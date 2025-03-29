02c09d860ea5fc83ca399e6e8638aa7e765f92a9
wenxiao Lv <2064747320@qq.com>
2025-03-29 17:12:30 +0800

    创建对战列表与排行榜页面

--- backendcloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
+++ backendcloud/backend/src/main/java/com/kob/backend/consumer/WebSocketServer.java
@@ -30,8 +30,7 @@ public class WebSocketServer {
     final public static ConcurrentHashMap<Integer, WebSocketServer> users = new ConcurrentHashMap<>();
     private User user;
     private Session session = null;
-
-    private static UserMapper userMapper;
+    public static UserMapper userMapper;
     public static RecordMapper recordMapper;
     private static BotMapper botMapper;
     public static RestTemplate restTemplate;
